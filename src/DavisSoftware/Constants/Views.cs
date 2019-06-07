c9eb24162fd64eb6b3c28effa01919c4e1cb9fd9
Paul Davis <pdavis86@hotmail.co.uk>
2019-06-07 22:14:24 +0100

    Made some progress

--- src/DavisSoftware/Constants/Views.cs
+++ src/DavisSoftware/Constants/Views.cs
@@ -7,5 +7,12 @@
             public const string Main = "~/Views/Shared/_Layout.cshtml";
         }
 
+        public static class Partials
+        {
+            public const string Header = "~/Views/Shared/_Header.cshtml";
+            public const string Navigation = "~/Views/Shared/_Nav.cshtml";
+            public const string Footer = "~/Views/Shared/_Footer.cshtml";
+        }
+
     }
 }
--- src/DavisSoftware/Controllers/BlogController.cs
+++ src/DavisSoftware/Controllers/BlogController.cs
@@ -12,27 +12,15 @@ namespace DavisSoftware.Controllers
     {
         public string Archive(DateTime? entryDate)
         {
-            return "You wants Blog Entry on Date:=" + entryDate.ToString();
+            return "You want Blog Entry on Date:=" + entryDate.ToString();
         }
 
-        public ActionResult Index(int? year, int? month) 
+        public ActionResult Index(int? year, int? month)
         {
-            //var face = new DavisSoftware.Models.BlogArticleModel()
-            //{
-            //    DateTime = DateTime.Parse("2010-06-04"),
-            //    Heading = "Lessons from support",
-            //    Content = "Here is a <strong>bold</strong> example"
-            //};
-
-            //TextWriter writer = new StreamWriter(@"C:\Temp\blah.xml");
-            //var ser = new XmlSerializer(typeof(DavisSoftware.Models.BlogArticleModel));
-            //ser.Serialize(writer, face);
-            //writer.Close();
-
-            var searchPattern = "";
-            searchPattern += year.HasValue ? year.Value.ToString("D4") : "";
-            searchPattern += month.HasValue ? month.Value.ToString("D2") : "";
-            searchPattern += "*";
+            var searchPattern =
+                (year.HasValue ? year.Value.ToString("D4") : "")
+                + (month.HasValue ? month.Value.ToString("D2") : "")
+                + "*";
 
             var posts = Directory.EnumerateFiles(Server.MapPath("/BlogData"), searchPattern);
 
@@ -40,9 +28,9 @@ namespace DavisSoftware.Controllers
             {
                 return new HttpNotFoundResult();
             }
-            if(posts.Count() > 1)
+            if (posts.Count() > 1)
             {
-                return new JsonResult() { Data = "There should ne be more than one" };
+                return new JsonResult() { Data = "There should not be more than one" };
             }
 
             var reader = new StreamReader(posts.First());
